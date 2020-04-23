e49e7deb5e58eb1acacba516c22dfa75f9046402
Dan Lepadatu <lepadatudan@gmail.com>
2020-04-24 00:27:21 +0200

    refactor: show ProfileDetails only on self evaluation page

--- frontend/src/components/Evaluation/SelfEvaluation/SelfEvaluation.js
+++ frontend/src/components/Evaluation/SelfEvaluation/SelfEvaluation.js
@@ -1,12 +1,33 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import EvaluationApi from "../../../api/evaluationApi";
 import EvaluationForm from "../evaluationForm";
+import ProfileApi from "../../../api/profileApi";
+import ProfileDetails from "../../Account/common/ProfileDetails/ProfileDetails";
+import ProfileOthers from "../../Account/common/ProfileOthers/ProfileOthers";
 
-const SelfEvaluation = () => (
-  <EvaluationForm
-    getForm={EvaluationApi.getEvaluationForm}
-    sendResults={EvaluationApi.sendSelfEvaluationResults}
-  />
-);
+const SelfEvaluation = () => {
+  const [userProfile, setUserProfile] = useState({});
+  const [dependants, setDependants] = useState([]);
+
+  useEffect(() => {
+    ProfileApi.get().then(setUserProfile);
+    ProfileApi.getDependants().then(setDependants);
+  }, []);
+
+  return (
+    <>
+      <EvaluationForm
+        getForm={EvaluationApi.getEvaluationForm}
+        sendResults={EvaluationApi.sendSelfEvaluationResults}
+      />
+
+      {userProfile.id && (
+        <ProfileDetails fields={userProfile} isSelf>
+          <ProfileOthers family={dependants} />
+        </ProfileDetails>
+      )}
+    </>
+  );
+};
 
 export default SelfEvaluation;
